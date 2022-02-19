ecce3f745e1953c8d791cc7469ee7035576ad214
Yestay <yestai777@gmail.com>
2022-02-19 13:48:15 +0600

    admin panel modified

--- scripts/admin_panel.js
+++ scripts/admin_panel.js
@@ -1,45 +1,123 @@
+var selected_product_type = "clothes";
+var categories;
+
 $(document).ready(function(){
-    $("#product_type").change(function(){
-        if($(this).val() == 'clothes'){
-            $("#shoes_material").hide();
-            $("#clothes_material").show();
-            $("#product_pattern_label").show();
-            $("#product_pattern").show();
+
+    fetch('/scripts/categories.json')
+  .then(response => response.json())
+  .then((data) =>{
+    categories = data;
+    
+    $("#subcategory").empty();
+    addOthersOption("subcategory");
+
+    for (let i = 0; i < categories["clothes_category"].length; i++){
+        addOption("subcategory", categories["clothes_category"][i]);
+    }
+
+    $("#product_brand").empty();
+    addOthersOption("product_brand");
+
+    for(let i = 0; i < categories["brand"].length; i++){
+        addOption("product_brand", categories["brand"][i]);    
+    }
+
+    $("#manufacturer").empty();
+
+    for(let i = 0; i < categories["countries"].length; i++){
+        let option = document.createElement("option");
+        option.value = categories["country_codes"][i];
+        option.text = categories["countries"][i];
+        $("#manufacturer").append(option);
+    }
+
+    $("#select_shoes_material").empty();
+    addOthersOption("select_shoes_material");
+
+    for(let i = 0; i < categories["shoes_material"].length; i++){
+        addOption("select_shoes_material", categories["shoes_material"][i]);
+    }
+
+    $("#sport_type").empty();
+    addOthersOption("sport_type");
+
+    for(let i = 0; i < categories["sport_type"].length; i++){
+        addOption("sport_type", categories["sport_type"][i]);
+    }
+
+    $("#select_merch").empty();
+    let option = document.createElement("option");
+    option.value = "None";
+    option.text = option.value;
+    $("#select_merch").append(option);
+    
+    for(let i = 0; i < categories["merch"].length; i++){
+        addOption("select_merch", categories["merch"][i]);
+    }
+  });
+
+  
+  $("#shoes_size").hide();
+  $("#accessory_size").hide();
+  $('#shoes_material').hide();
+
+    $("#clothes_btn").click(function(){
+        showClothes();
+    });
+
+    $("#accessories_btn").click(function(){
+        showAccessories();
+    });
+
+    $("#shoes_btn").click(function(){
+        showShoes();
+    });
+
+    $("#subcategory").change(function(){
+        if($(this).val() == "Другое"){
+            $("#other_subcategory").show();
         }
-        else{
-            $("#shoes_material").show();
-            $("#clothes_material").hide();
-            $("#product_pattern_label").hide();
-            $("#product_pattern").hide();
+        else {
+            $("#other_subcategory").hide();
         }
     });
 
-    $("#category_gender").change(function(){
+    $("#other_shoes_material").show();
+    $("#select_shoes_material").change(function(){
+        if($(this).val() == "Другое"){
+            $("#other_shoes_material").show();
+        }
+        else{
+            $("#other_shoes_material").hide();
+        }
+    });
 
-        if($(this).val() == 'male'){
-            $("#subcategory_male").show();
-            $("#subcategory_female").hide();
-            $("#subcategory_kids").hide();
-            $("#subcategory_multisex").hide();
-            
+    $("#product_brand").change(function(){
+        if($(this).val() == "Другое"){
+            $("#other_brand").show();
         }
-        else if($(this).val() == 'female'){
-            $("#subcategory_male").hide();
-            $("#subcategory_female").show();
-            $("#subcategory_kids").hide();
-            $("#subcategory_multisex").hide();
+        else {
+            $("#other_brand").hide();
         }
-        else if($(this).val() == 'kids'){
-            $("#subcategory_male").hide();
-            $("#subcategory_female").hide();
-            $("#subcategory_kids").show();
-            $("#subcategory_multisex").hide();
+    });
+
+    $("#sport_style_div").hide();
+
+    $("#product_style").change(function(){
+        if($(this).val() == "Спортивный"){
+            $("#sport_style_div").show();
         }
-        else if($(this).val() == 'multisex'){
-            $("#subcategory_male").hide();
-            $("#subcategory_female").hide();
-            $("#subcategory_kids").hide();
-            $("#subcategory_multisex").show();
+        else {
+            $("#sport_style_div").hide();
+        }
+    });
+
+    $("#sport_type").change(function(){
+        if($(this).val() == "Другое"){
+            $("#other_sport_type").show();
+        }
+        else {
+            $("#other_sport_type").hide();
         }
     });
 });
@@ -47,24 +125,21 @@ $(document).ready(function(){
 var materialCount = 2;
 
 function addMaterial(){
-    let sel = document.createElement("select");
-    sel.id = "clothes_material" + materialCount;
     
-    let opt = document.createElement("option");
-    opt.value = "value";
-    opt.text = "text";
-
-    sel.append(opt);
-
+    let sel = document.createElement("input");
+    sel.id = "clothes_material" + materialCount;
+    sel.type = "text";
+    sel.placeholder = "Материал";
     var inp = document.createElement("input");
     inp.type = "number";
     inp.id = "clothes_material_percent" + materialCount;
     inp.placeholder = "процентов";
 
+    sel.style = "width:40%";
+    inp.style = "width: 40%";
     $("#clothes_material").append(sel);
     
     $("#clothes_material").append(inp);
-    console.log(materialCount);
     materialCount++;
 }
 
@@ -75,7 +150,91 @@ function removeLastMaterial(){
         return;
     }
     $("input").remove("#clothes_material_percent" + materialCount);
-    $("select").remove("#clothes_material" + materialCount);
+    $("input").remove("#clothes_material" + materialCount);
     
-    console.log(materialCount);
+}
+
+function addOthersOption(id){
+    let option = document.createElement("option");
+    option.value = "Другое";
+    option.text = option.value;
+    $("#" + id).append(option);
+}
+
+function addOption(id, value){
+    let option = document.createElement("option");
+    option.value = value;
+    option.text = option.value;
+    $("#" + id).append(option);
+}
+
+function showClothes(){
+    $("#shoes_material").hide();
+        $("#clothes_material").show();
+        $("#accessory_size").hide();
+        $("#shoes_size").hide();
+        $("#clothes_size").show();
+        $("#pattern_div").show();
+        selected_product_type = "clothes";
+        $("#clothes_btn").addClass("selected_product_type");
+        $("#shoes_btn").removeClass("selected_product_type");
+        $("#accessories_btn").removeClass("selected_product_type");
+
+        $("#subcategory").empty();
+        addOthersOption("subcategory");
+
+        for (let i = 0; i < categories["clothes_category"].length; i++){
+            let option = document.createElement("option");
+            option.value = categories["clothes_category"][i];
+            option.text = option.value;
+            $("#subcategory").append(option);
+        }
+}
+
+function showAccessories(){
+    $("#shoes_material").hide();
+    $("#clothes_material").hide();
+    $("#accessory_size").show();
+    $("#shoes_size").hide();
+    $("#clothes_size").hide();
+    $("#pattern_div").hide();
+    selected_product_type = "accessories";
+    $("#accessories_btn").addClass("selected_product_type");
+    $("#shoes_btn").removeClass("selected_product_type");
+    $("#clothes_btn").removeClass("selected_product_type");
+
+    $("#subcategory").empty();
+    addOthersOption("subcategory");
+
+    for (let i = 0; i < categories["accessories_category"].length; i++){
+        let option = document.createElement("option");
+        option.value = categories["accessories_category"][i];
+        option.text = option.value;
+        
+        $("#subcategory").append(option);
+    }
+}
+
+function showShoes(){
+    $("#shoes_material").show();
+    $("#clothes_material").hide();
+        $("#accessory_size").hide();
+        $("#shoes_size").show();
+        $("#clothes_size").hide();
+        $("#pattern_div").hide();
+        selected_product_type = "shoes";
+        $("#shoes_btn").addClass("selected_product_type");
+        $("#accessories_btn").removeClass("selected_product_type");
+        $("#clothes_btn").removeClass("selected_product_type");
+
+        $("#subcategory").empty();
+        addOthersOption("subcategory");
+
+        for (let i = 0; i < categories["shoes_category"].length; i++){
+            let option = document.createElement("option");
+            option.value = categories["shoes_category"][i];
+            option.text = option.value;
+            
+            $("#subcategory").append(option); 
+        }
 }
