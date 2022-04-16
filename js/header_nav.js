5217dfce87f077303f7e1f6f3f7fac28624a96e1
SmokeMkee <83024391+SmokeMkee@users.noreply.github.com>
2022-04-16 21:53:47 +0600

    footer ifo add , nav bug fix

--- js/header_nav.js
+++ js/header_nav.js
@@ -50,20 +50,20 @@ $(".navbar-toggler").click(function(){
 
 
 
-// --------------add active class-on another-page move----------
-jQuery(document).ready(function($){
-	// Get current path and find target link
-	var path = window.location.pathname.split("/").pop();
+// // --------------add active class-on another-page move----------
+// jQuery(document).ready(function($){
+// 	// Get current path and find target link
+// 	var path = window.location.pathname.split("/").pop();
 
-	// Account for home page with empty path
-	if ( path == '' ) {
-		path = 'index.html';
-	}
+// 	// Account for home page with empty path
+// 	if ( path == '' ) {
+// 		path = 'index.html';
+// 	}
 
-	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
-	// Add active class to target link
-	target.parent().addClass('active');
-});
+// 	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+// 	// Add active class to target link
+// 	target.parent().addClass('active');
+// });
 
 
 
